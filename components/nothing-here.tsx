08d694d6b369c72baa38ed30f2c562093ccbb806
manuel-heav <manuelheav32@gmail.com>
2024-10-28 11:03:53 +0300

    Apostrophe fixed

--- components/nothing-here.tsx
+++ components/nothing-here.tsx
@@ -5,7 +5,7 @@ const NothingHere = () => {
   return (
       <div className="flex flex-col items-center justify-center h-[50vh] text-center px-4">
         <Ghost className="w-24 h-24 text-muted-foreground mb-4 animate-bounce" />
-        <h2 className="text-2xl font-bold mb-2">Oops! It's a ghost town here!</h2>
+        <h2 className="text-2xl font-bold mb-2">Oops! It`&apos;s a ghost town here!</h2>
         <p className="text-muted-foreground">
           Be the first person to post under this tag.
         </p>
