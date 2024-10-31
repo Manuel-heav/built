6c88971008f296beb289b75c962f9cf95879380f
manuel-heav <manuelheav32@gmail.com>
2024-10-31 09:11:09 +0300

    added resource and bot tag

--- constants/index.ts
+++ constants/index.ts
@@ -1,156 +1,126 @@
-import { ProjectType, TagTypes } from "@/types";
+import { TagTypes } from "@/types";
 
 export const tags: TagTypes[] = [
   {
-    id: 1,
+    id: Math.random() * 1000,
     title: "4DBV",
     url: "/DB2AV",
   },
   {
-    id: 2,
+    id: Math.random() * 1000,
     title: "Software",
     url: "/software",
   },
   {
-    id: 3,
+    id: Math.random() * 1000,
     title: "UI/UX",
     url: "/ui",
   },
   {
-    id: 4,
+    id: Math.random() * 1000,
     title: "Libraries",
     url: "/libraries",
   },
   {
-    id: 5,
+    id: Math.random() * 1000,
     title: "Packages",
     url: "/packages",
   },
   {
-    id: 6,
+    id: Math.random() * 1000,
+    title: "Resource",
+    url: "/resources",
+  },
+  {
+    id: Math.random() * 1000,
+    title: "Bots",
+    url: "/bots",
+  },
+  {
+    id: Math.random() * 1000,
     title: "Tools",
     url: "/tools",
   },
   {
-    id: 12,
+    id: Math.random() * 1000,
     title: "E-commerce",
     url: "/ecommerce",
   },
   {
-    id: 13,
+    id: Math.random() * 1000,
     title: "Open Source",
     url: "/open-source",
   },
   {
-    id: 14,
+    id: Math.random() * 1000,
     title: "API",
     url: "/api",
   },
   {
-    id: 15,
+    id: Math.random() * 1000,
     title: "Data Science",
     url: "/data-science",
   },
   {
-    id: 16,
+    id: Math.random() * 1000,
     title: "ML",
     url: "/machine-learning",
   },
   {
-    id: 17,
+    id: Math.random() * 1000,
     title: "Blockchain",
     url: "/blockchain",
   },
   {
-    id: 18,
+    id: Math.random() * 1000,
     title: "Security",
     url: "/security",
   },
   {
-    id: 20,
+    id: Math.random() * 1000,
     title: "Automation",
     url: "/automation",
   },
   {
-    id: 24,
+    id: Math.random() * 1000,
     title: "Gaming",
     url: "/gaming",
   },
   {
-    id: 25,
+    id: Math.random() * 1000,
     title: "Entertainment",
     url: "/entertainment",
   },
   
   {
-    id: 26,
+    id: Math.random() * 1000,
     title: "AI",
     url: "/ai",
   },
   {
-    id: 27,
+    id: Math.random() * 1000,
     title: "Cloud",
     url: "/cloud",
   },
   {
-    id: 28,
+    id: Math.random() * 1000,
     title: "Mobile",
     url: "/mobile",
   },
   {
-    id: 29,
+    id: Math.random() * 1000,
     title: "Desktop",
     url: "/desktop",
   },
   {
-    id: 30,
+    id: Math.random() * 1000,
     title: "Web",
     url: "/web",
   },
   {
-    id: 31,
+    id: Math.random() * 1000,
     title: "VR/AR",
     url: "/vr-ar",
   },
 ];
 
-
-// export const projects: ProjectType[] = [
-//   {
-//     id: 2,
-//     imageUrl: "/images/screenshot1.png",
-//     title: "Shad Cn UI",
-//     tags: ["UI/UX", "Tools"],
-//     github_repo: "https://github.com",
-//     live_demo: "https://shadcn-ui.vercel.app",
-//     telegram_channel: "https://t.me",
-//     description: "This is a description",
-//     likes: 100,
-//     comments: 20,
-//   },
-//   {
-//     id: 4,
-//     imageUrl: "/images/screenshot2.png",
-//     title: "Shad Cn UI",
-//     tags: ["UI/UX", "Tools"],
-//     github_repo: "https://github.com",
-//     live_demo: "https://shadcn-ui.vercel.app",
-//     telegram_channel: "https://t.me",
-//     description: "This is a description long description and long description",
-//     likes: 100,
-//     comments: 20,
-//   },
-//   {
-//     id: 6,
-//     imageUrl: "/images/screenshot3.png",
-//     title: "Shad Cn UI",
-//     tags: ["UI/UX", "Tools"],
-//     github_repo: "https://github.com",
-//     live_demo: "https://shadcn-ui.vercel.app",
-//     telegram_channel: "https://t.me",
-//     description:
-//       "This is a description long description and long description long description and long descriptionlong description and long descriptionlong description and long descriptionlong description and long descriptionlong description and long descriptionlong description and long descriptionlong description and long descriptionlong description and long description",
-//     likes: 100,
-//     comments: 20,
-//   },
-// ];
