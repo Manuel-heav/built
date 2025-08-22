4fbc247f5e2fb17eff6f3efad7a94f6e2df48d07
manuel-heav <manuelheav32@gmail.com>
2025-08-22 16:44:27 +0300

    Fix: changing output type to standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isCI = Boolean(process.env.CI);
 const nextConfig = {
   images: {
     domains: ['miftztgfemnglhjovkgg.supabase.co','salonlfc.com','minio.theblogrammer.com'],
   },
-  // Avoid Windows symlink issues locally; still use standalone in CI
-  ...(isCI ? { output: 'standalone' } : {}),
+  output: "standalone",
 };
 
 export default nextConfig;
